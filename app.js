9721f95033dc9b3515f7c44d1ae8b476f4e84920
Scott Pierce <scottpiercew@gmail.com>
2017-06-16 16:49:45 -0400

    finished step 7

---
+++ app.js
@@ -0,0 +1,18 @@
+let superheroData = [
+  { name: 'Superman', phoneNumber: '248-909-1170', comicsAffiliation: 'DC Comics', img_url: 'https://vignette4.wikia.nocookie.net/superman/images/b/b1/Superman_Action_976_Gary_Frank.png/revision/latest/scale-to-width-down/288?cb=20170501140424'},
+  { name: 'Aquaman', phoneNumber: '619-246-0021', comicsAffiliation: 'DC Comics', img_url: 'https://i956.photobucket.com/albums/ae50/Eyeballed/aquaman.jpg'},
+  { name: 'Wonder Woman', phoneNumber: '909-345-7181', comicsAffiliation: 'DC Comics', img_url: 'http://pm1.narvii.com/6310/35b20d479ed02e62e05d33f3eade72d3182d1a95_hq.jpg'},
+  { name: 'Spider-Man', phoneNumber: '111-7171-6049', comicsAffiliation: 'Marvel Comics', img_url: 'https://i.annihil.us/u/prod/marvel/i/mg/2/00/53710b14a320b.png'},
+  { name: 'Iron Man', phoneNumber: '787-494-3052', comicsAffiliation: 'Marvel Comics', img_url: 'https://www.sideshowtoy.com/wp-content/uploads/2016/04/captain-america-civil-war-iron-man-xlvi-sixth-scale-marvel-silo-902708.png'}
+]
+
+angular
+  .module("superheroApp", [])
+  .controller("SuperheroController", [
+    SuperheroControllerFunc
+  ])
+
+function SuperheroControllerFunc () {
+  this.superheroes = superheroData
+
+}
